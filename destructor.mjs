50ae3549874a088039d4a5af900004f41048aedd
ayushkli86 <katuwalayush616@gmail.com>
2026-05-07 11:23:01 +0545

    new commit

---
+++ destructor.mjs
@@ -0,0 +1,21 @@
+
+//array
+// let [a,b,c] =[10 ,11,12]
+// console.log(a,b,c)
+// console.log(a)
+// console.log(b)
+// console.log(c)
+// console.log(a,b,b,c,c,a,b,c,a,b,c)
+
+
+//oject 
+
+let{name,status,surname}={
+    name:"ayush",
+    stauts:"unmarried",
+    surname:"katuwal"
+}
+console.log(name)
+console.log(status)
+console.log(surname)
+console.log(name,status)
