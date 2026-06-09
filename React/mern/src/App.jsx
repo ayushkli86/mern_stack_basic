ccd3a305b7e17d1eb4a9f73e1bebe8fc137018f0
ayushkli86 <katuwalayush616@gmail.com>
2026-06-09 12:32:22 +0545

    Add React mern project

---
+++ React/mern/src/App.jsx
@@ -0,0 +1,16 @@
+const App =()=>{
+  let name = "ayush";
+  let surname = "katuwal";
+
+  return <div>
+
+    <p>
+      ronish jhutke piche pagal he 
+    </p>  
+    <p>instagram id dedo isko koi</p>
+    <p> my name is {name} and the surname is {surname} and 1+1 is {1+1-1*1}</p>
+    <img src ="image.png" width ="200px" height ="200px" ></img>
+  </div>
+
+};
+export default App
